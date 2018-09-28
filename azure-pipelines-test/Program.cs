cda41aff9a4b797c01ecf243f81b9f7d6d09a422
Tobias Hein <Tobias.Hein@p-und-z.de>
2018-09-28 15:05:47 +0200

    Initial commit

---
+++ azure-pipelines-test/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace azure_pipelines_test
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
